a46cd38d0a7b86f2cc6282ce0771b9f64e53e602
Derrick Schneider <derrick@Derrick.local>
2017-07-06 19:19:48 -0700

    Add ability to parse files and some sample dictionaries

--- playfair.go
+++ playfair.go
@@ -1,20 +1,49 @@
 package main
 
 import (
+  "bufio"
   "fmt"
+  "os"
+  
   "playfair_solver/wordsquare"
 )
 
 // This is specifically geared towards the Hills of Homicied problem
-// in P&A July/August 2017
+// in P&A July/August 2017.
+// Usage: playfair_solver keyword_file plaintext_file ciphertext_to_match
 func main() {
-   // args = plaintext digraph,ciphertext digraph, etc.
-   // e.g.: playfair_solver FI,HX QZ,TV
-   
-   // for each line in stdin, create a word sqaure for it
-   // then encrypt each digraph against word square and
-   // see if it maps to the equivalent ciphertext. If all parse,
-   // match.
-   square := wordsquare.KeySquareFromString("monarchy")
-   fmt.Println(square.EncryptString("wearediscoveredsaveyourself"))
+  keywordFileName := os.Args[1]
+  plaintextFileName := os.Args[2]
+  ciphertext := os.Args[3]
+  
+  keywordFile, err := os.Open(keywordFileName)
+  panicOnError(err)
+  defer keywordFile.Close()
+    
+  keywordScanner := bufio.NewScanner(keywordFile)
+  for keywordScanner.Scan() {
+    keyword := keywordScanner.Text()
+    square := wordsquare.KeySquareFromString(keyword)
+
+    // now go through each plain text guess and compare encoded version
+    // to ciphertext
+    plaintextFile, err := os.Open(plaintextFileName)
+    panicOnError(err)
+    plaintextScanner := bufio.NewScanner(plaintextFile)
+    for plaintextScanner.Scan() {
+      plaintext := plaintextScanner.Text()
+      encrypted := square.EncryptString(plaintext)
+      if encrypted == ciphertext {
+        fmt.Printf("keyword: %s, plaintext: %s, encrypted: %s\n", keyword, plaintext, encrypted)
+      }
+      
+    }
+    plaintextFile.Close()    
+  }     
+}
+
+func panicOnError(e error) {
+  if e != nil {
+      panic(e)
+  }
 }
